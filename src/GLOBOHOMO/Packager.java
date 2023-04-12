f888d5e1cb188b36d6b63c1a5a27015d22466b38
Nicholas <nporter108@gmail.com>
2023-04-12 15:01:31 -0400

    One last thing.

--- src/GLOBOHOMO/Packager.java
+++ src/GLOBOHOMO/Packager.java
@@ -16,37 +16,39 @@ public class Packager implements Runnable{
     public void run() {
         int returnedValue;
         Random rand = new Random();
-        if(iQ.lockingMechanism.tryLock()){
-            if(sQ.lockingMechanism.tryLock()){
-                try {
-                    returnedValue = iQ.consume();
-                    System.out.println("Using " + returnedValue);
-                    String pack = "<" + returnedValue + ">";
-                    System.out.println("Packaging" + returnedValue);
-                    sQ.produce(pack);
-                    sQ.lockingMechanism.unlock();
-                } catch (Exception e) {
-                    System.out.println("Holdup!");
+        int i = 0;
+        while(i < 10) {
+            if (iQ.lockingMechanism.tryLock()) {
+                if (sQ.lockingMechanism.tryLock()) {
+                    try {
+                        returnedValue = iQ.consume();
+                        System.out.println("Using " + returnedValue);
+                        String pack = "<" + returnedValue + ">";
+                        System.out.println("Packaging" + returnedValue);
+                        sQ.produce(pack);
+                        sQ.lockingMechanism.unlock();
+                    } catch (Exception e) {
+                        System.out.println("Holdup!");
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException ex) {
+                            System.out.println(ex);
+                        }
+                    }
+                    iQ.lockingMechanism.unlock();
+                } else {
+                    iQ.lockingMechanism.unlock();
                     try {
                         Thread.sleep(10);
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
+                    } catch (InterruptedException e) {
+                        System.out.println(e);
                     }
                 }
-                iQ.lockingMechanism.unlock();
-            }
-            else {
-                iQ.lockingMechanism.unlock();
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(rand.nextInt(51) + 20);
                 } catch (InterruptedException e) {
                     System.out.println(e);
                 }
-            }
-            try {
-                Thread.sleep(rand.nextInt(51) + 20);
-            } catch (InterruptedException e) {
-                System.out.println(e);
             }
         }
     }
